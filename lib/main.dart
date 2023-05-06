761ace44d8c0956f299684721cb952379fbb1f84
Roman <rkukoyev@mail.ru>
2023-05-07 01:13:52 +0600

    v1.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:record_mp3/record_mp3.dart';
+import 'package:govorilka/ui/RecordingService.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:record_mp3/record_mp3.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  MyApp({super.key});
+  RecordingService service = RecordingService();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,15 +17,18 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(
+        recorderService: service,
+      ),
     );
   }
 }
 
+// ignore: must_be_immutable
 class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.recorderService});
   int _index = 0;
-  final String title;
+  RecordingService recorderService;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -33,25 +37,33 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    Permission.microphone.request();
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        backgroundColor: const Color.fromARGB(255, 238, 61, 61),
+        title: const Text("Speech recorder"),
       ),
-      body: ListView.builder(
-        itemCount: widget._index,
-        itemBuilder: (context, index) => ListTile(
-          tileColor: Colors.grey,
-          leading: Text("$index"),
-          trailing: const Icon(Icons.mic),
-          minVerticalPadding: 12.0,
+      body: Center(
+        child: Column(
+          children: [
+            Text("Try to record your speech"),
+            Expanded(
+              child: FloatingActionButton(
+                onPressed: (){
+                  setState(() {
+                    widget.recorderService.stopRecord();
+                  });
+                },
+                backgroundColor: const Color.fromARGB(255, 236, 85, 85),
+                child: Icon(widget.recorderService.isRecording 
+                  ? Icons.pause 
+                  : Icons.play_arrow,
+                ),
+              ), 
+            ),
+          ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-          // widget._index++;
-          //onCreateDialog().showDialog()
-        }),
-      ),
+      )
     );
   }
 }
