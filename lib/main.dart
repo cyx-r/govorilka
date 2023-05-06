a613ec2ca0665dd506c6b87ec2c79c08d45a3710
cyx-r <rkukoyev@gmail.com>
2023-05-06 12:19:36 +0300

    v1.0.0

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:record_mp3/record_mp3.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: 4,
+        itemBuilder: (context, index) => ListTile(
+          tileColor: Colors.grey,
+          leading: Text("$index"),
+          trailing: const Icon(Icons.mic),
+          minVerticalPadding: 12.0,
+        ),
+      ),
+    );
+  }
+}
