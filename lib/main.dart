b212fabdd8c378731e7c1002e1818850e5b39ede
cyx-r <rkukoyev@gmail.com>
2023-05-07 10:06:46 +0300

    v2.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:govorilka/ui/RecordingService.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:record_mp3/record_mp3.dart';
+import 'package:govorilka/service/RecordingService.dart';
+import 'package:govorilka/ui/LoginPage.dart';
+import 'package:govorilka/ui/MyHomePage.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-  RecordingService service = RecordingService();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,53 +16,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(
-        recorderService: service,
-      ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class MyHomePage extends StatefulWidget {
-  MyHomePage({super.key, required this.recorderService});
-  int _index = 0;
-  RecordingService recorderService;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    Permission.microphone.request();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 238, 61, 61),
-        title: const Text("Speech recorder"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Text("Try to record your speech"),
-            Expanded(
-              child: FloatingActionButton(
-                onPressed: (){
-                  setState(() {
-                    widget.recorderService.stopRecord();
-                  });
-                },
-                backgroundColor: const Color.fromARGB(255, 236, 85, 85),
-                child: Icon(widget.recorderService.isRecording 
-                  ? Icons.pause 
-                  : Icons.play_arrow,
-                ),
-              ), 
-            ),
-          ],
-        ),
-      )
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/home': (context) => MyHomePage()
+      },
     );
   }
 }
